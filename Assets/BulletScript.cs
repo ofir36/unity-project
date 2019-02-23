814fa4abfa93886efed8ed0e089e51ec99b5a53e
Unknown <ofir36@gmail.com>
2019-02-23 19:11:51 +0200

    work

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision");
+        if (collision.collider.tag == "Enemy")
+        {
+            Debug.Log("true");
+            StartCoroutine(collision.collider.GetComponent<ExplodeScript>().SplitMesh(true));
+        }
+    }
+}
---
+++ Assets/FadeEffectScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeEffectScript : MonoBehaviour
+{
+    public Text hellText;
+    public float fadeSpeed = 5;
+    public bool enterance = false;
+    void Start()
+    {
+        hellText.color = UnityEngine.Color.white;
+    }
+
+    void Update()
+    {
+        ColorChange();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+            enterance = true;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+            enterance = false;
+    }
+
+    private void ColorChange()
+    {
+        if (enterance)
+            hellText.color = UnityEngine.Color.Lerp(hellText.color, UnityEngine.Color.white, fadeSpeed * Time.deltaTime);
+        if (!enterance)
+            hellText.color = UnityEngine.Color.Lerp(hellText.color, UnityEngine.Color.clear, fadeSpeed * Time.deltaTime);
+    }
+}
