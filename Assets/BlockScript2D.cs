5fb45705ba36342c6b12ecb14855a4699eb80733
Unknown <ofir36@gmail.com>
2019-02-23 20:35:26 +0200

    start of 2d scene

---
+++ Assets/BlockScript2D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript2D : MonoBehaviour
+{
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ColorScript.OnColorChange += ColorScript_OnColorChange;
+        ColorScript_OnColorChange(ColorScript.ActiveColor);
+    }
+
+    void ColorScript_OnColorChange(Color newColor)
+    {
+        var renderer = GetComponent<MeshRenderer>();
+        if (newColor == color)
+        {
+            GetComponent<BoxCollider2D>().enabled = true;
+            var c = renderer.material.color;
+            renderer.material.color = new UnityEngine.Color(c.r, c.g, c.b, 1);
+        }
+        else
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+            var c = renderer.material.color;
+            renderer.material.color = new UnityEngine.Color(c.r, c.g, c.b, 0.2f);
+        }
+    }
+}
